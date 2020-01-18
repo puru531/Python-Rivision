81867569ab5f2f40d8da8d1e87ac3555ab894b16
Purushottam Kumar <42888056+puru531@users.noreply.github.com>
2020-01-19 00:41:00 +0530

    Add files via upload

---
+++ Day 1 Print_Comment_EscapeSequence.py
@@ -0,0 +1,20 @@
+#In Python, new line is automtically added in print command
+print("I can do this!")
+
+""" This is multiline comment 
+To write two lines in same single line use ,end="" command"""
+
+print("I you will try",end=" ")
+print("You will be succeed")
+
+#or
+print("If you will try","You will be succeed")
+print("If you try","You will be succeed", end="")
+print("Printing a number as string i.e 1154521")
+
+#escape sequnce
+print("Never mind, \nDon't give up.")#\n creates a new line.
+print("This is escape seque\\nce")#escape sequence allow us to print \n
+print("Don\'t")#for single quote and \" for double qoute
+print("Don't do \n silly things\t1")#\n for new line and \t for tab
+
