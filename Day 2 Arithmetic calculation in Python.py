18ea0ea301d31cd0c14913f32ee1100c18956ea3
Purushottam Kumar <42888056+puru531@users.noreply.github.com>
2020-01-20 00:14:12 +0530

    Add files via upload

---
+++ Day 2 Arithmetic calculation in Python.py
@@ -0,0 +1,8 @@
+print(20+65+485)#Addition
+print(989-65)#Subtraction
+print(89*59)#multiplication
+print(15*6+56*9+36*8)#multiplication and addition
+print(89/5)#division with quotient as decimal number
+print(89//5)#division without decimal value
+print(29%5)#modulus ---> it is used to print the remainder
+print(21**2)#exponential
