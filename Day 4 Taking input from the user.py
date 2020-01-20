69e4f7108e89a58c1f2b633931d9bab4898f3ace
Purushottam Kumar <42888056+puru531@users.noreply.github.com>
2020-01-21 01:42:03 +0530

    Add files via upload

---
+++ Day 4 Taking input from the user.py
@@ -0,0 +1,13 @@
+#We can take input from the user with the help of input() command
+#By default, input value is of string type
+
+
+#program to add two numbers which is taken as input frm the user.
+
+print("Write the first number :")
+firstnum=int(input()) #taking input and typecating it into integer
+
+print("Write the second number: ")
+secondnum=int(input())
+
+print("Sum of these two numbers is: ",firstnum+secondnum)
