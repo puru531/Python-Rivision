baf9f1e073656d479401686c892a8bbe63fadb68
Purushottam Kumar <42888056+puru531@users.noreply.github.com>
2020-01-21 01:41:30 +0530

    Add files via upload

---
+++ Day 3 Variables, Data types and Typecasting.py
@@ -0,0 +1,35 @@
+#varibles are containers to hold the values and store it in memory at particular address.
+#Python is dynamic language so we don't need to define the variable with data types.
+
+var="Puru !"  #defining a variable and assigning value to it.
+print(var) #assecing and printing the variable
+
+#String values are written within  " "
+
+#when we add two string variables, it get concatenated.
+#When we add two integer variables it gives its sum.
+
+var1="Hello "  #String
+var2="85 "    #string
+var3="65"
+
+var4=98
+var5=23
+
+print(var1 + var)
+print(var2 + var3)  #it will concatenate
+print(var4 + var5) #it will give sum
+
+#Datatypes----->
+#we can know the datatype of any variable by:
+print(type(var1)) #string
+print(type(var4)) #integer
+
+
+
+#Typecasting ------>
+#changing an datatype of variable is called typecasting
+print(int(var2)+int(var3)) #string value is changed to integer and prints its sum
+
+
+
