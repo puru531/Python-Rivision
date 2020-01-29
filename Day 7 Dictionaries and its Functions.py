bda0e83d3db0834b6ac6e78698a5dba50eb64bc8
Purushottam Kumar <42888056+puru531@users.noreply.github.com>
2020-01-29 22:29:26 +0530

    Add files via upload

---
+++ Day 7 Dictionaries and its Functions.py
@@ -0,0 +1,33 @@
+#It is a data type of Dictionary
+#An ordered collection of data in key:value pair form
+#Collection of pairs are enclosed in curly braces
+
+#defining a dictionary
+
+d = {}
+print(type(d))
+
+d1 = {"Pen":"Ink","Pencil":"Sharpner","Colour":{"Dark":"Balck","Light":"White"}} #We can aso insert a dictionary into a dictionary
+print(d1)
+print(d1["Pen"]) #dictionary slicing
+print(d1["Colour"])
+print(d1["Colour"]["Dark"])#printing value of a dictionary in a dictionary'
+
+#Updating dictionary
+d1["Laptop"] = "Charger"
+print(d1)
+
+#updating multiple values at one time
+d1.update({"Phone":"Sim","Sony":"Speaker"})
+print(d1)
+d3=d1 #It will craete a reference of d1. Any change in d3 will affect d1.
+print(d3)
+
+d2=d1.copy()#it will create  a copy of d1 changes in d2 will not effect d1
+del d2["Pen"]#Deleting the key and value of Pen
+print(d2)
+print(d1)
+
+print(d1.keys())#printing all the keys
+print(d1.values())#printing all the values
+print(d2.items())#printing all key value pairs
