e8b41797af0a13087661e1b69bd60b09590f2ec8
Purushottam Kumar <42888056+puru531@users.noreply.github.com>
2020-01-30 22:26:06 +0530

    Update Day 8 Question on Dictionary.py

--- Day 8 Question on Dictionary.py
+++ Day 8 Question on Dictionary.py
@@ -1,5 +1,6 @@
-#Create a dictionary and take input from the user to find its value.
+#  Q --->   Create a dictionary and take input from the user to find its value.
 
+#Solutio ----->
 d1 = {"Immutable":"The which cannot be changed.","Mutable":"That which cannot be changed","String":"Collection of characters","Integer":"Number"}#declaring dictionary
 print("Enter any word (Immutable, Mutable, String, Integer) : ")
 p=input()#Taking input from the user
