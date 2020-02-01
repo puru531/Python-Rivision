02bb450042655b0a1bfb70acdcb20c07af46b41a
Purushottam Kumar <42888056+puru531@users.noreply.github.com>
2020-02-02 01:06:16 +0530

    Update Day 10 If Else and Elif conditionals.py

--- Day 10 If Else and Elif conditionals.py
+++ Day 10 If Else and Elif conditionals.py
@@ -1,1 +1,12 @@
 #If, else and elif return boolean result as true or false
+
+
+age = int(input("What is your age? : "))
+if age<5 or age>120:
+    print("You are underage or overage.")
+elif age>18:
+    print("You are eligible for driving licence.")
+elif age<18:
+    print("You are not eligible for driving licence.")
+else:
+    print("I cannot decide.")
