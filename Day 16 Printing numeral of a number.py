1d730960ad999ea46b341774578f7c120ef5ccb9
Purushottam Kumar <42888056+puru531@users.noreply.github.com>
2020-02-20 00:21:46 +0530

    Update and rename Printing numeral of a number.py to Day 16 Printing numeral of a number.py

--- Day 16 Printing numeral of a number.py
+++ Day 16 Printing numeral of a number.py
@@ -1,4 +1,4 @@
-#Write a program to print numeral of a 4 digit number
+#Write a program to convert a 4 digit number into words
 numm = 1000
 while(numm<=9999):
     thsnd={"0":"Zero", "1":"One", "2":"Two", "3":"Three", "4":"Four", "5":"Five", "6":"Six", "7":"Seven", "8":"Eight", "9":"Nine"}
